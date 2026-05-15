60ce5baee2f4fda4504d75b1c0bf78b46aabafd1
Your Name <your.email@example.com>
2026-05-15 17:05:08 +0530

    use env vars for supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rxcqqojbwbuzzdmgbaqz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4Y3Fxb2pid2J1enpkbWdiYXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MTAwNTQsImV4cCI6MjA5NDM4NjA1NH0.D49cWlmsSRHDiEqNnOKwaTj0_TFsta5qBdgefkWdigQ'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rxcqqojbwbuzzdmgbaqz.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4Y3Fxb2pid2J1enpkbWdiYXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MTAwNTQsImV4cCI6MjA5NDM4NjA1NH0.D49cWlmsSRHDiEqNnOKwaTj0_TFsta5qBdgefkWdigQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
